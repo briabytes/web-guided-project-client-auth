e403eb7502cd3fa5d07aa8b982691c3854c34a81
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-15 13:26:22 -0500

    lecture work-in-progress

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,12 @@ import axios from 'axios';
 function App() {
   const logout = () => {
     axios
-      .post("http://localhost:5000/api/logout", {
-        headers: {
-          Authorization: localStorage.getItem("token")
-        }
-      })
-      .then((res)=>{
+      .post("http://localhost:5000/api/logout", {userToken:localStorage.getItem('token')})
+      .then((res) => {
         console.log(res);
       })
-      .catch(err=>{
-        console.log(err);
-      })
+      .catch((err) => console.log(err));
   };
-
 
   return (
     <Router>
